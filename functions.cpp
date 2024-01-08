ac04a423c268d5b6ba768a447bb35019312cc86d
ViniciusBenetti <makako1289@gmail.com>
2024-01-08 14:49:34 -0300

    c,python e c++  ao mesmo tempo na internet

---
+++ functions.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+extern "C"{
+void bilhaoc(){
+    int i =0;
+    while(i < 1000000000){
+        i++;
+    }
+}
+void bilhaocpp(){
+    int i =0;
+    while(i < 1000000000){
+        i++;
+    }
+    
+}
+}
--- index.py
+++ index.py
@@ -1,18 +1,44 @@
 from flask import Flask
+import ctypes
+import time
 
+i = 0
+
+def bilhaopython():
+    global i
+    for _ in range(1000000000):
+        i += 1
 
 app = Flask(__name__)
 
+lib = ctypes.CDLL("./python-flask/functions.so")
 
-@app.route("/")
-def hello():
-    return "hello"
+print("comecou")
 
-@app.route("/bye")
-def bye():
-    return "bye world"
+@app.route("/python")
+def umbilhaopython():
+    global i
+    i = 0 
+    start_time = time.time()
+    bilhaopython()
+    exec_time = time.time()
+    return "1 bilhão em python: " + str(exec_time - start_time) + " segundos"
 
+@app.route("/c")
+def umbilhaoc():
+    start_time = time.time()
+    lib.bilhaoc()
+    exec_time = time.time()
+    return "1 bilhão em c: " + str(exec_time - start_time) + " segundos"
+
+@app.route("/cpp")
+def umbilhaocpp():
+    start_time = time.time()
+    lib.bilhaocpp()
+    exec_time = time.time()
+    return "1 bilhão em cpp: " + str(exec_time - start_time) + " segundos"
 
 if __name__ == "__main__":
     app.run()
+
 
